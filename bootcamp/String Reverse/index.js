e62a784173444949d7ff759ec77d87e72f2448b6
Moshiur Rahman <rmoshiur705@gmail.com>
2020-12-28 09:42:32 +0600

    Added two different solution for String Reverse

--- bootcamp/String Reverse/index.js
+++ bootcamp/String Reverse/index.js
@@ -1,9 +1,31 @@
+
+// By using for loop
 function reverse(str) {
     let reversedStr = '';
-    for (let i = str.length - 1; i >= 0; i--) {
-        reversedStr += str[i];
+    // Traditional for Loop
+    // for (let i = str.length - 1; i >= 0; i--) {
+    //     reversedStr += str[i];
+    // }
+
+    // For off Loop
+    for (const char of str) {
+        reversedStr = char + reversedStr;
     }
     return reversedStr;
+}
+
+// By Using Array reverse (Js)
+
+function reverse(str) {
+    return str.split('').reverse().join('');
+}
+
+// By Using Array reduce
+
+function reverse(str) {
+    return str.split('').reduce((reversed, char) =>
+        char + reversed, ''
+    );
 }
 
 reverse('Hello My dear');
