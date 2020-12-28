497f168c5448980ca17eb79dbbc1366b44831795
Moshiur Rahman <rmoshiur705@gmail.com>
2020-12-28 09:17:37 +0600

    Added Project Structure && String Reverse

---
+++ bootcamp/String Reverse/index.js
@@ -0,0 +1,9 @@
+function reverse(str) {
+    let reversedStr = '';
+    for (let i = str.length - 1; i >= 0; i--) {
+        reversedStr += str[i];
+    }
+    return reversedStr;
+}
+
+reverse('Hello My dear');
