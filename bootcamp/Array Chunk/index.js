0d7a823ef4c6172ac2df3e866ec2201f91716d3b
Moshiur Rahman <rmoshiur705@gmail.com>
2020-12-29 11:01:09 +0600

    Added Solution Array Chunk Problem

---
+++ bootcamp/Array Chunk/index.js
@@ -0,0 +1,35 @@
+//My solution
+function arrayChunk(array, size) {
+    // if (array.length < size) return array;
+    let chunkArray = [];
+    let newArr = [];
+    for (let index = 0; index < array.length; index++) {
+        newArr.push(array[index]);
+        if ((index + 1) % size == 0) {
+            chunkArray.push(newArr);
+            newArr = [];
+        }
+    }
+    if (newArr.length) {
+        chunkArray.push(newArr);
+    }
+
+    return chunkArray;
+}
+
+//alternative solution
+function arrayChunk(array, size) {
+    const chunked = [];
+    let index = 0;
+
+    while (index < array.length) {
+        chunked.push(array.slice(index, index + size));
+        index += size;
+    }
+
+    return chunked;
+
+}
+
+
+console.log(arrayChunk([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 9));
