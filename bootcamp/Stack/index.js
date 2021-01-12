a31b138d68949f9e53e95b01174b0159b183d7e2
Moshiur Rahman <rmoshiur705@gmail.com>
2021-01-12 22:50:07 +0600

    Implemented Stack

---
+++ bootcamp/Stack/index.js
@@ -0,0 +1,24 @@
+class Stack {
+    constructor() {
+        this.data = [];
+    }
+
+    add(item) {
+        this.data.push(item);
+    }
+
+    remove() {
+        return this.data.pop();
+    }
+
+    getLastElm() {
+        return this.data[this.data.length - 1];
+    }
+}
+
+let stack = new Stack();
+stack.add(1);
+stack.add(2);
+stack.getLastElm();
+stack.remove();
+console.log(stack.getLastElm());
